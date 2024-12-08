007fe383e4db7709cd770f4938c2f111b93d569c
Ratshalingwa Adivhaho <ratshalingwaadivhaho106@gmail.com>
2024-12-08 14:51:21 +0200

    Update images.js

--- src/images/images.js
+++ src/images/images.js
@@ -3,19 +3,19 @@ const Image = [
     photo: require('../assets/images/projects/postme.png'),
     name: 'Blog Web App',
     description: 'A simple Spring Boot web application that allows users to post and manage content.',
-    link: 'https://github.com/RACHA011/PostItWebApp'
+    link: 'https://post-it-web-app.onrender.com'
   },
   {
     photo: require('../assets/images/projects/api.png'),
     name: 'RachaRestAPI',
     description: 'RachaRestAPI is a secure RESTful API built with Spring Boot for user account management and authentication.',
-    link: 'https://github.com/RACHA011/RachaRestAPI'
+    link: 'https://restdev-01.onrender.com/swagger-ui/index.html#'
   },
   {
     photo: require('../assets/images/projects/mantistemplate.png'),
     name: 'Photo Showcase Web App',
     description: 'A front-end application using React to interact with the RachaRestAPI.',
-    link: 'https://github.com/RACHA011/PhotoVault-v01'
+    link: 'https://photo-vault.vercel.app'
   }
 ];
 
